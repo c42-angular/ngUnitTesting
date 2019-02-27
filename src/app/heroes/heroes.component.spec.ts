42dc7ada001c79b3aa2cf6ac4c02bb1c8cf2692c
Arunkumar Ramachandriya <arun@arunkumars-mbp.home>
2019-02-27 21:03:17 +0000

    Interation expectation: expect(s.delete).toHaveBeenCalledWith(HEROES[2])

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -28,5 +28,14 @@ describe('HeroesComponent', () => {
 
       expect(component.heroes.length).toBe(2);
     });
+
+    it('should call deleteHero on HeroService', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+    });
   });
 });
