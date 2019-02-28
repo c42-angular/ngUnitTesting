1402bdb20aa38475cd0333d49bd9db8dab3c9850
Arunkumar Ramachandriya <arun@arunkumars-mbp.home>
2019-02-28 14:37:27 +0000

    Call change detection manually for binding to happen and we can test UI

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -14,7 +14,7 @@ describe('Hero Component (Shallow)', () => {
     // this acts as the resolution context for directives etc
     TestBed.configureTestingModule({
       declarations: [ HeroComponent ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+      schemas: [ NO_ERRORS_SCHEMA ] // tells Angular to ignore any unknown directives & elements, in this case the routerLink
     });
 
     fixture = TestBed.createComponent(HeroComponent);
@@ -23,6 +23,7 @@ describe('Hero Component (Shallow)', () => {
   it('template should display name of hero that was set', () => {
     // get hold of the actual wrapped component
     fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+    fixture.detectChanges(); // force bind - in a normal run this will happen in automatic change detection
 
     const neA = fixture.nativeElement.querySelector('a');
     expect(neA.textContent).toContain('SuperDude');
